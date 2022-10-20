71059f4f9588723b2abf097fbd9c681249588dc0
Robbie Hockley <robbie.hockley@gmail.com>
2022-10-20 22:16:49 +0800

    final upload

--- leafletPart1/static/js/logic.js
+++ leafletPart1/static/js/logic.js
@@ -59,13 +59,13 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojs
 
   // add a GeoJSON layer to the map once the file is loaded.
   L.geoJson(data, {
-    // We turn each feature into a circleMarker on the map.
+    // turn each feature into a circleMarker on the map.
     pointToLayer: function(feature, latlng) {
       return L.circleMarker(latlng);
     },
-    // We set the style for each circleMarker using our styleInfo function.
+    // set the style for each circleMarker
     style: styleInfo,
-    // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+    // create a popup for each marker
     onEachFeature: function(feature, layer) {
       layer.bindPopup(
         "Magnitude: "
@@ -78,5 +78,27 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojs
     }
   }).addTo(map);
 
-  
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = ["#2c99ea", "#2ceabf", "#92ea2c", "#d5ea2c","#eaa92c", "#ea2c2c"];
+
+
+  // loop through the intervals of colors to put it in the label
+    for (var i = 0; i<grades.length; i++) {
+      div.innerHTML +=
+      "<i style='background: " + colors[i] + "'></i> " +
+      grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+
+  };
+
+  // Finally, we our legend to the map.
+  legend.addTo(map);
 });
